edd4417d0bc458f2083f475aafa58be036bccd19
BekaMujirishvili <115090497+BekaMujirishvili@users.noreply.github.com>
2023-09-24 03:28:18 +0400

    migration update

--- quickqueues/application/migrations/20230430121800_create_portal_user_settings.php
+++ quickqueues/application/migrations/20230430121800_create_portal_user_settings.php
@@ -1,26 +1,41 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-class Create_portal_user_settings extends CI_Migration {
+class Migration_Create_portal_user_settings extends CI_Migration {
 
     public function up()
     {
         $this->dbforge->add_field(
             array(
-                'call_overload' => array(
-                        'type' => 'int',
-                        'constraint' => 255,
-                        'null' => false,
+                'name' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
                 ),
-                'sms_content' => array(
-                        'type' => 'varchar',
-                        'constraint' => 255,
-                        'null' => true,
-                )
+                'value' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 300,
+                ),
+                'default' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 300
+                ),
             )
         );
         $this->dbforge->create_table('qq_portal_user_settings');
+
+        $data[] = array(
+            'name'      => 'call_overload',
+            'value'     => '0',
+            'default'   => '10'
+        );
+
+        $data[] = array(
+            'name'      => 'sms_content',
+            'value'     => '',
+            'default'   => 'sms content'
+        );
+        
+        $this->db->insert_batch('qq_portal_user_settings', $data);
 
     }
 
