07cb73198e385ea59816cbefff4b1c99cdf06048
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-05 22:57:08 +0400

    Update stats.js

--- quickqueues/assets/js/components/queues/stats.js
+++ quickqueues/assets/js/components/queues/stats.js
@@ -103,7 +103,7 @@ var queue_stats = new Vue({
                                     data: dataset_outgoing,
                                 },
                                 {
-                                    label: lang['calls_missed'],
+                                    label: lang['ringnoanswer'],
                                     backgroundColor: get_colors('red'),
                                     data: dataset_missed,
                                 },
