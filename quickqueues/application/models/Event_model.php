aca537c904e0250a386138048e411ac2d2a29c8e
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-07 19:22:04 +0400

    Update Event_model.php

--- quickqueues/application/models/Event_model.php
+++ quickqueues/application/models/Event_model.php
@@ -117,7 +117,7 @@ class Event_model extends MY_Model {
             return false;
         }
         $this->db->select('agent_id');
-        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 1 THEN uniqueid END) AS calls_missed');
+        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 10 THEN uniqueid END) AS calls_missed');
         $this->db->where_in('queue_id', $queue_ids);
         $this->db->where('date >', $date_range['date_gt']);
         $this->db->where('date <', $date_range['date_lt']);
@@ -147,7 +147,7 @@ class Event_model extends MY_Model {
             return false;
         }
         $this->db->select('agent_id');
-        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 1 THEN uniqueid END) AS calls_missed');
+        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 10 THEN uniqueid END) AS calls_missed');
         $this->db->select('SUM(IF(event_type = "STOPPAUSE", pausetime, 0)) AS total_pausetime');
         $this->db->where_in('agent_id', $agent_id);
         $this->db->where('date >', $date_range['date_gt']);
@@ -163,7 +163,7 @@ class Event_model extends MY_Model {
     
         $this->db->select('DATE_FORMAT(date, "%H") AS hour');
         $this->db->select('agent_id');
-        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 1 THEN uniqueid END) AS calls_missed');
+        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 10 THEN uniqueid END) AS calls_missed');
         $this->db->where_in('agent_id', $agent_id);
         $this->db->where('date >', $date_range['date_gt']);
         $this->db->where('date <', $date_range['date_lt']);
@@ -182,7 +182,7 @@ class Event_model extends MY_Model {
 
         $this->db->select('DATE_FORMAT(date, "%Y-%m-%d") AS date');
         $this->db->select('agent_id');
-        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 1 THEN uniqueid END) AS calls_missed');
+        $this->db->select('COUNT(DISTINCT CASE WHEN event_type = "RINGNOANSWER" AND qq_events.ringtime > 10 THEN uniqueid END) AS calls_missed');
         $this->db->where_in('agent_id', $agent_id);
         $this->db->where('date >', $date_range['date_gt']);
         $this->db->where('date <', $date_range['date_lt']);
