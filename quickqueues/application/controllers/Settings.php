1ea8d4979788e530d1a58e15fc25add1571a52bd
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-13 15:24:25 +0400

    Update Settings.php

--- quickqueues/application/controllers/Settings.php
+++ quickqueues/application/controllers/Settings.php
@@ -1,22 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 class Settings extends CI_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
 
+        $this->load->model('Settings_model');
+        $this->load->model('Queue_model');
+        
         $this->app_language = $this->Config_model->get_item('app_language');
+        
         if ($this->app_language) 
         {
-          $this->lang->load(array('main', 'help'), $this->app_language);
+            $this->lang->load(array('main', 'help'), $this->app_language);
         } 
         else 
         {
-          $this->lang->load(array('main', 'help'), 'english');
+            $this->lang->load(array('main', 'help'), 'english');
         }
 
         $this->data = new stdClass();
@@ -25,29 +27,23 @@ class Settings extends CI_Controller
     
     public function index()
     {
-        $this->load->model('Settings_model');
-
- 
         $settings_data           = $this->Settings_model->getSettings();
         $queues                  = $this->Queue_model->get_queue_entries();
         $settings_data['queues'] = $queues;
         $this->data->settings    = $settings_data;
        
- 
         load_views('settings/index', $this->data, true);
     }
 
     public function update_settings()
     {
-        if($this->input->post())
+        if ($this->input->post())
         {
-            $this->load->model('Settings_model');
-
             $newOverload   = $this->input->post('overload');
             $newSmsContent = $this->input->post('sms_text');
             $smsKey        = $this->input->post('sms_key');
             $smsType       = $this->input->post('sms_type');
-            $queueId       = $this->input->post('queue_id');
+            $queueId       = $this->input->post('selected_queue_id'); // Updated from 'queue_id'
 
             $this->Settings_model->updateSettings('call_overload', $newOverload);
             $this->Settings_model->updateSettings('sms_content', $newSmsContent);
