7bc2bf2456708bbb3f63213cdd4b7437bf522ce4
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-14 15:20:21 +0400

    Create 20230430121920_create_sms_logs.php

---
+++ quickqueues/application/migrations/20230430121920_create_sms_logs.php
@@ -0,0 +1,56 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_qq_sms_logs extends CI_Migration {
+
+    public function up()
+    {
+        $fields = array(
+            'id' => array(
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE,
+            ),
+            'sms_content' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ),
+            'sms_token' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ),
+            'sms_type' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ),
+            'queue_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ),
+            'status' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+            ),
+        );
+
+        $this->dbforge->add_field($fields);
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('sms_logs');
+
+        $data[] = array(
+            'sms_content' => '',
+            'sms_token'   => '',
+            'sms_type'    => '',
+            'queue_id'    => '',
+            'status'      => '',
+        );
+
+        $this->db->insert_batch('qq_sms_logs', $data);
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('qq_sms_logs');
+    }
+}
