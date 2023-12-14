ca26c66e1eeabb8914e311a2a72f08da3c542ff0
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-14 17:01:42 +0400

    migration class update

--- quickqueues/application/migrations/20230430121940_create_sms_logs.php
+++ quickqueues/application/migrations/20230430121940_create_sms_logs.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Create_qq_sms_logs extends CI_Migration 
+class Migration_Create_sms_logs extends CI_Migration 
 {
 
     public function up()
