b4502324bf3eece06581873eebd66a0665279fc2
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-14 15:37:42 +0400

    Update 20230430121920_create_sms_logs.php

--- quickqueues/application/migrations/20230430121920_create_sms_logs.php
+++ quickqueues/application/migrations/20230430121920_create_sms_logs.php
@@ -5,38 +5,39 @@ class Migration_Create_qq_sms_logs extends CI_Migration {
 
     public function up()
     {
-        $fields = array(
-            'id' => array(
-                'type'       => 'INT',
-                'constraint' => 11,
-                'unsigned' => TRUE,
-                'auto_increment' => TRUE,
-            ),
-            'sms_content' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200,
-            ),
-            'sms_token' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200,
-            ),
-            'sms_type' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200,
-            ),
-            'queue_id' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200,
-            ),
-            'status' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 200,
-            ),
+        $this->dbforge->add_field(
+            array(
+                'id' => array(
+                    'type'       => 'INT',
+                    'constraint' => 11,
+                    'unsigned' => TRUE,
+                    'auto_increment' => TRUE,
+                ),
+                'sms_content' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'sms_token' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'sms_type' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'queue_id' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'status' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+            )
         );
 
-        $this->dbforge->add_field($fields);
         $this->dbforge->add_key('id', TRUE);
-        $this->dbforge->create_table('sms_logs');
+        $this->dbforge->create_table('qq_sms_logs');
 
         $data[] = array(
             'sms_content' => '',
