f2c06bd9b2857e51aeff129dd8806a5f2975e1cd
TeonaKavzinadze <teonakavzinadze@gmail.com>
2023-12-18 09:38:37 +0400

    Update Settings.php

--- quickqueues/application/controllers/Settings.php
+++ quickqueues/application/controllers/Settings.php
@@ -31,29 +31,30 @@ class Settings extends CI_Controller
         $queues                  = $this->Queue_model->get_queue_entries();
         $settings_data['queues'] = $queues;
         $this->data->settings    = $settings_data;
-       
         load_views('settings/index', $this->data, true);
+    }
+
+    public function get_settings()
+    {
+        $queue_id = $this->input->get('queue_id');
+        $settings = $this->Settings_model->getSettings($queue_id);
+        $this->output->set_content_type('application/json')->set_output(json_encode($settings));
     }
 
     public function update_settings()
     {
         if ($this->input->post())
         {
-            $newOverload   = $this->input->post('overload');
-            $newSmsContent = $this->input->post('sms_text');
-            $smsKey        = $this->input->post('sms_key');
-            $smsType       = $this->input->post('sms_type');
-            $queueId       = $this->input->post('selected_queue_id'); // Updated from 'queue_id'
-            $status        = $this->input->post('status');
-
-            $this->Settings_model->updateSettings('call_overload', $newOverload);
-            $this->Settings_model->updateSettings('sms_content', $newSmsContent);
-            $this->Settings_model->updateSettings('sms_token', $smsKey);
-            $this->Settings_model->updateSettings('sms_type', $smsType);
-            $this->Settings_model->updateSettings('queue_id', $queueId);
-            $this->Settings_model->updateSettings('status', $status);
-
-            redirect('settings/index');
+            $data = array(
+                'call_overload' => $this->input->post('overload'),
+                'sms_content'   => $this->input->post('sms_text'),
+                'sms_token'     => $this->input->post('sms_key'),
+                'sms_type'      => $this->input->post('sms_type'),
+                'queue_id'      => $this->input->post('selected_queue_id'),
+                'status'        => $this->input->post('status'),
+            );
+            $this->Settings_model->updateSettings($data);
         }
     }
 }
+
