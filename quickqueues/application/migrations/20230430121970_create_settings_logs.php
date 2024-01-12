4e2c16dd3bd82c24dd163cef4fb5a9f26b9311bf
TeonaKavzinadze <teonakavzinadze@gmail.com>
2024-01-12 14:05:43 +0400

    Create 20230430121970_create_settings_logs.php

---
+++ quickqueues/application/migrations/20230430121970_create_settings_logs.php
@@ -0,0 +1,57 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_settings_logs extends CI_Migration 
+{
+
+    public function up()
+    {
+        $this->dbforge->add_field(
+            array(
+                'id' => array(
+                    'type'       => 'INT',
+                    'constraint' => 11,
+                    'unsigned' => TRUE,
+                    'auto_increment' => TRUE,
+                ),
+                'sms_content' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'sms_token' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'sms_type' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'queue_id' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'status' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 200,
+                ),
+                'call_overload'=> array(
+                    'type'           => 'INT',
+                    'constraint'     => 11,
+                    'unsigned'       => TRUE,
+                    'default'        => 0,
+                ),
+            )
+        );
+
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('qq_settings_logs');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('qq_settings_logs');
+    }
+}
+
+
+
