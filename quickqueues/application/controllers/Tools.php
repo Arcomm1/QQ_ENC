eb418ab78c0408950795ff766cc9f9bd0641d52d
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-01-25 21:45:50 +0400

    Update Tools.php

--- quickqueues/application/controllers/Tools.php
+++ quickqueues/application/controllers/Tools.php
@@ -220,6 +220,18 @@ parser_unlock_complex();
 
 					$this->db->where('timestamp >', $last_parsed_event);
 					$this->db->delete('qq_events');
+                    
+					$uniqueid = $this->db->select('uniqueid')->from('qq_calls')->where('timestamp', $last_parsed_event)->get()->row()->uniqueid;
+					
+					// Check if $uniqueid is not empty
+					if (!empty($uniqueid)) {
+						// Update the userfield in the cdr table where uniqueid is greater than $uniqueid
+						$this->db->set('userfield', '')
+								 ->where('uniqueid >', $uniqueid)
+								 ->where('userfield', 'QQCOLLECTED')
+								 ->update('asteriskcdrdb.cdr');
+					}                    
+                    
 				} else {
 					log_message('error', 'Invalid last_parsed_event timestamp, cannot perform deletion.');
 				}
@@ -296,6 +308,21 @@ parser_unlock_complex();
             exit();
         }
         $send_sms_on_exit_event = $this->Config_model->get_item('app_send_sms_on_exit_event');
+
+		foreach ($qq_agents as $qq_agent) {
+			// Step 2: Check if there is a matching name in users table with the same extension
+			$extension = $qq_agent->extension;
+			$matching_user = $this->db->get_where('users', array('extension' => $extension))->row();
+
+			if ($matching_user) {
+				// Step 3: Update qq_agents name and display_name to name from users table
+				$this->db->where('id', $qq_agent->id);
+				$this->db->update('qq_agents', array(
+					'name' => $matching_user->name,
+					'display_name' => $matching_user->name // You can update display_name to the same value as name
+				));
+			}
+		}        
 
             try 
     {		
