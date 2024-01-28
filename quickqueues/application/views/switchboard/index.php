989c30f6d2bf29d5181014a15a89fedbe95e97e2
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-01-28 21:05:37 +0400

    Update index.php

--- quickqueues/application/views/switchboard/index.php
+++ quickqueues/application/views/switchboard/index.php
@@ -1,23 +1,24 @@
-  <div id="switchboard">
-    <div class="card border-top-primary border-primary border-top-3">
-      <div class="card-header">{{ lang['switchboard'] }}</div>
-      <div class="card-body">
-        <div class="extension-states">
-          <div v-for="state in extension_states" :key="state[0]" class="extension-state">
-            <div class="card" :class="state_class_map[state[1].Status]">
-              <div class="card-body">
-                <h4 class="card-title">{{ state[0] }}</h4>
-                <p class="card-text">
-                  <span v-if="state[1].Status === 2" class="alert-icon">!</span>
-                  {{ devices[state[0]] }}
-                </p>
-              </div>
+<div id="switchboard">
+  <div class="card border-top-primary border-primary border-top-3">
+    <div class="card-header">{{ lang['switchboard'] }}</div>
+    <div class="card-body">
+      <div class="extension-states">
+        <div v-for="state in extension_states" :key="state[0]" class="extension-state">
+          <div class="card" :class="state_class_map[state[1].Status]">
+            <div class="card-body">
+              <h4 class="card-title">{{ state[0] }}</h4>
+              <p class="card-text">
+                <span v-if="state[1].Status === 2" class="alert-icon">!</span>
+                {{ devices[state[0]] || 'N/A' }}
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
+
   
   <style>
 .extension-states {
