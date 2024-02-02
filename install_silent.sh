c34bbc265c51509d90d35c7b27322a7812577eef
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-02 23:18:32 +0400

    Update install_silent.sh

--- install_silent.sh
+++ install_silent.sh
@@ -177,7 +177,7 @@ fi
 ln -s $(pwd)/quickqueues/assets/img/favicon.ico /var/www/html/favicon.ico
 
 # Permissions for local key store for JS
-sudo chown asterisk:asterisk $(pwd)/quickqueues/persist.json
+# sudo chown asterisk:asterisk $(pwd)/quickqueues/persist.json
 
 echo "====== INSTALLATION SUMMARY ===================================================="
 
