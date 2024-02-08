1c474eac8e312e6ea3d4385e0fe099df858606ec
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-08 16:48:49 +0400

    Update 20230430121999_update_settings_logs_4.php

--- quickqueues/application/migrations/20230430121999_update_settings_logs_4.php
+++ quickqueues/application/migrations/20230430121999_update_settings_logs_4.php
@@ -5,15 +5,18 @@ class Migration_update_settings_logs_4 extends CI_Migration
 {
     public function up()
     {
-        $fields = array(
-            'queue_id' => array(
-                'type' => 'INT',
-                'constraint' => 11,
-            ),
-        );
-        $this->dbforge->add_column('qq_settings_logs', $fields);
+        // Check if 'queue_id' column exists before trying to add it
+        if (!$this->db->field_exists('queue_id', 'qq_settings_logs')) {
+            $fields = array(
+                'queue_id' => array(
+                    'type' => 'INT',
+                    'constraint' => 11,
+                ),
+            );
+            $this->dbforge->add_column('qq_settings_logs', $fields);
+        }
 
-        // Use $this->db->field_exists() to check if the column exists
+        // Use $this->db->field_exists() to check if other columns exist before dropping them
         if ($this->db->field_exists('calls_without_service_queue_id', 'qq_settings_logs')) 
         {
             $this->dbforge->drop_column('qq_settings_logs', 'calls_without_service_queue_id');
@@ -27,6 +30,6 @@ class Migration_update_settings_logs_4 extends CI_Migration
 
     public function down()
     {
-      $this->dbforge->drop_table('qq_settings_logs');
+        // Add the logic to revert the 'up' method changes if necessary
     }
 }
