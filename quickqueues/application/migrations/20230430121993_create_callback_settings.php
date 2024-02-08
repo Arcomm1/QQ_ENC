8e2045c2a4adec4d2e0dbae618faab9bbe7bf730
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-08 15:38:03 +0400

    Update 20230430121993_create_callback_settings.php

--- quickqueues/application/migrations/20230430121993_create_callback_settings.php
+++ quickqueues/application/migrations/20230430121993_create_callback_settings.php
@@ -3,10 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migration_Create_callback_settings extends CI_Migration 
 {
-
     public function up()
     {
-        $fields = array (
+        $fields = array(
             'id' => array(
                 'type' => 'INT',
                 'constraint' => 11,
@@ -35,25 +34,25 @@ class Migration_Create_callback_settings extends CI_Migration
                 'constraint' => 10,
             ),
             'resolution' => array(
-                'type'=> 'VARCHAR',
+                'type' => 'VARCHAR',
                 'constraint' => 255,
             ),
             'date' => array(
                 'type' => 'TIMESTAMP',
                 'default' => 'CURRENT_TIMESTAMP',
-                'on update CURRENT_TIMESTAMP' => TRUE,
             ),
         );
 
         $this->dbforge->add_field($fields);
         $this->dbforge->add_key('id', TRUE);
-        $this->dbforge->create_table('qq_callback_settings');
+        $this->dbforge->create_table('qq_callback_settings', TRUE);
+
+        // Add the SQL query to modify the 'date' column
+        $this->db->query('ALTER TABLE qq_callback_settings MODIFY COLUMN date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
     }
 
     public function down()
     {
         $this->dbforge->drop_table('qq_callback_settings');
     }
-
 }
-
