d76d9630dcfe87547c3591f3ee7e5e00ae14945b
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-08 16:46:51 +0400

    Update 20230430121999_update_settings_logs_4.php

--- quickqueues/application/migrations/20230430121999_update_settings_logs_4.php
+++ quickqueues/application/migrations/20230430121999_update_settings_logs_4.php
@@ -13,12 +13,13 @@ class Migration_update_settings_logs_4 extends CI_Migration
         );
         $this->dbforge->add_column('qq_settings_logs', $fields);
 
-        if ($this->dbforge->field_exists('calls_without_service_queue_id', 'qq_settings_logs')) 
+        // Use $this->db->field_exists() to check if the column exists
+        if ($this->db->field_exists('calls_without_service_queue_id', 'qq_settings_logs')) 
         {
             $this->dbforge->drop_column('qq_settings_logs', 'calls_without_service_queue_id');
         }
 
-        if ($this->dbforge->field_exists('date', 'qq_settings_logs')) 
+        if ($this->db->field_exists('date', 'qq_settings_logs')) 
         {
             $this->dbforge->drop_column('qq_settings_logs', 'date');
         }
