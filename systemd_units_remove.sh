7e92474cb05b8b745d04f5932bed9d97cb570d00
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-09 15:20:41 +0400

    Update systemd_units_remove.sh

--- systemd_units_remove.sh
+++ systemd_units_remove.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Stop and disable timer unit
-systemctl stop QQclearcache_get_all_cached.timer
-systemctl disable QQclearcache_get_all_cached.timer
+# Files locations
+SCRIPT="/home/get_all_cached.sh"
+CRON_JOB="/etc/cron.d/get_all_cached"
+LOG="/home/get_all_cached_pid"
 
-# Remove the service and timer unit files
-rm /etc/systemd/system/QQclearcache_get_all_cached.service
-rm /etc/systemd/system/QQclearcache_get_all_cached.timer
+echo "Removing script and cron job files if they exist."
+rm -f "$SCRIPT" "$CRON_JOB" "$LOG"
 
-# Define file paths and names as variables
-source_file="/usr/src/QQ/quickqueues/assets/js/components/monitoring/index_for_normal.js"
-target_file="/usr/src/QQ/quickqueues/assets/js/components/monitoring/index.js"
-
-# Copy the source file to the target file with force overwrite
-cp -f "$source_file" "$target_file"
-
-# Reload systemd
-systemctl daemon-reload
+echo "Cleanup completed."
