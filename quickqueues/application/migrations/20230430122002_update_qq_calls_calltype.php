552ae0eb892983b396fe69a873a000d5f8dd7e84
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-12 05:07:52 +0400

    Update 20230430122002_update_qq_calls_calltype.php

--- quickqueues/application/migrations/20230430122002_update_qq_calls_calltype.php
+++ quickqueues/application/migrations/20230430122002_update_qq_calls_calltype.php
@@ -5,26 +5,29 @@ class Migration_update_qq_calls_calltype extends CI_Migration {
 
     public function up()
     {
+        // Common SRC condition for all updates
+        $srcCondition = "(src IN (SELECT extension FROM qq_agents) 
+                          OR src IN (SELECT extension FROM users) 
+                          OR src IN (SELECT extension FROM qq_agents_archived))";
+        
         // Update to 'local'
         $this->db->query("
             UPDATE qq_calls 
             SET calltype = 'local' 
             WHERE 
-                (src IN (SELECT extension FROM qq_agents) 
-                OR src IN (SELECT extension FROM users) 
-                OR src IN (SELECT extension FROM qq_agents_archived)) 
+                $srcCondition
             AND 
                 (dst IN (SELECT extension FROM qq_agents) 
                 OR dst IN (SELECT extension FROM users) 
                 OR dst IN (SELECT extension FROM qq_agents_archived))
         ");
 
-        // Update to 'local_abandoned'
+        // Update to 'local_abandoned' (Note: src condition is already included, dst condition adapted for consistency)
         $this->db->query("
             UPDATE qq_calls 
             SET calltype = 'local_abandoned' 
             WHERE 
-                (src IN (SELECT extension FROM users) OR src IN (SELECT extension FROM qq_agents_archived)) 
+                $srcCondition
                 AND (dst IS NULL OR dst = '') 
                 AND event_type = 'ABANDON' 
                 AND agent_id = 0
@@ -35,16 +38,16 @@ class Migration_update_qq_calls_calltype extends CI_Migration {
             UPDATE qq_calls 
             SET calltype = 'local_queue' 
             WHERE 
-                src IN (SELECT extension FROM users) 
+                $srcCondition
                 AND dst IN (SELECT extension FROM queues_config)
         ");
-		
+        
         // Update to 'local_fcode'
         $this->db->query("
             UPDATE qq_calls 
             SET calltype = 'local_fcode' 
             WHERE 
-                src IN (SELECT extension FROM users) 
+                $srcCondition
                 AND dst LIKE '%*%'
         ");
     }
@@ -52,7 +55,6 @@ class Migration_update_qq_calls_calltype extends CI_Migration {
     public function down()
     {
         // Optionally, add logic to revert the changes made in this migration.
-        // This might involve setting calltype back to its original value or a default one,
-        // but that depends on your application's needs and whether you can accurately revert these changes.
+        // Reverting data updates can be complex and depends on the original state of your data.
     }
 }
