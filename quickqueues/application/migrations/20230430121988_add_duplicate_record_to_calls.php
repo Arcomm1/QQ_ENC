4508a4cb3f0e3633f1a222f2fd2ad15348feb365
Arcomm1 <142622740+Arcomm1@users.noreply.github.com>
2024-02-21 22:23:07 +0400

    Add files via upload

--- quickqueues/application/migrations/20230430121988_add_duplicate_record_to_calls.php
+++ quickqueues/application/migrations/20230430121988_add_duplicate_record_to_calls.php
@@ -1,20 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 class Migration_add_duplicate_record_to_calls extends CI_Migration {
-
 
     public function up()
     {
-        $fields = array(
-            'duplicate_record' => array(
-                'type' => 'VARCHAR',
-                'constraint' => 3,
-                'default' => NULL
-            )
-        );
+        // Check if the 'duplicate_record' column already exists in the 'qq_calls' table
+        if (!$this->db->field_exists('duplicate_record', 'qq_calls')) {
+            $fields = array(
+                'duplicate_record' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => 3,
+                    'default' => NULL
+                )
+            );
 
-        $this->dbforge->add_column('qq_calls', $fields);
+            $this->dbforge->add_column('qq_calls', $fields);
+        }
     }
 }
